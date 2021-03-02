510023b4dc611a6f27bcb3d4e34e611b25e9bbb0
Lionel Noronha <lionelnoronha1@gmail.com>
2021-03-02 14:09:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+
+import discord
+import os
+from discord.ext import commands
+import lol
+
+lol = (https://github.com/ggrammar/pizzapi.git)
+num1 = False
+address1 = ""
+name1 = ""
+credit1 = ""
+Bot = commands.Bot(command_prefix='$')
+client = discord.Client
+store = address1.closest_store()
+menu = store.get_menu
+
+@Bot.command()
+async def menu(ctx , item ):
+  ki == menu.search(item)
+  await ctx.send(ki)
+
+  
+@Bot.command()
+async def foo(ctx, name, address , credit):
+  address1 == address 
+  name1 == name
+  credit1 == credit
+  num1 == True  
+
+@Bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+Bot.run(os.getenv('TOKEN'))
+client.run (os.getenv("TOKEN"))
